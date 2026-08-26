1d2a2ce9cc599d044f2c312a2d1cf22c7ba10eac
Mithran <mithuoffical06@gmail.com>
2026-08-26 14:15:09 +0530

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -3,45 +3,43 @@ import sqlite3
 conn = sqlite3.connect("database.db")
 
 conn.execute("""
-CREATE TABLE jobs(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-title TEXT,
-company TEXT,
-description TEXT
+CREATE TABLE IF NOT EXISTS jobs(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    company TEXT,
+    description TEXT
 )
 """)
 
 conn.execute("""
-CREATE TABLE candidates(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-name TEXT,
-email TEXT,
-password TEXT,
-resume TEXT
+CREATE TABLE IF NOT EXISTS candidates(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    email TEXT,
+    password TEXT,
+    resume TEXT
 )
 """)
 
 conn.execute("""
-CREATE TABLE applications (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-candidate_id TEXT,
-job_id TEXT,
-status TEXT
+CREATE TABLE IF NOT EXISTS applications(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    candidate_id TEXT,
+    job_id TEXT,
+    status TEXT,
+    interview_date TEXT
 )
 """)
 
 conn.execute("""
-CREATE TABLE recruiters(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-username TEXT,
-password TEXT
+CREATE TABLE IF NOT EXISTS recruiters(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    password TEXT
 )
 """)
 
-conn = sqlite3.connect("database.db")
-conn.execute("ALTER TABLE applications ADD COLUMN interview_date TEXT")
 conn.commit()
 conn.close()
-
 
 print("Database Created Successfully")
